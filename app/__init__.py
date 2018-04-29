7a780bf5248328213a7fdc180854a9328442b7af
Daniel Santos <dsantosp12@gmail.com>
2018-04-29 15:13:52 -0400

    Make ping timeout 30000 ms

--- app/__init__.py
+++ app/__init__.py
@@ -24,7 +24,7 @@ db = MySQLDatabase(config.DB_NAME, user=config.DB_USERNAME,
                    password=config.DB_PASSWORD, host=config.DB_HOST, port=3306)
 bcrypt = Bcrypt(app)
 jwt = JWTManager(app)
-socketio = SocketIO(app)
+socketio = SocketIO(app=app, ping_timeout=30000)
 
 from app.resources import chat
 
